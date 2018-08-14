8cdb1ec3d2f7b6aa03c141cfbccde1a4ec90b1b1
Kyrylo Yakymenko <yakimych@gmail.com>
2018-08-14 15:08:15 +0200

    Move components into separate folder

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,13 @@
-import * as React from 'react';
-import './App.css';
-
-import logo from './logo.svg';
+import * as React from "react";
+import "./App.css";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Bench Press: Road to 100</h1>
+          {/* TODO: Fetch from the API */}
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.

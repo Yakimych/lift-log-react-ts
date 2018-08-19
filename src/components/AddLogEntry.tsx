ee04a58db0f87839983c0d08f3d4ad4152718ea9
Kyrylo Yakymenko <yakimych@gmail.com>
2018-08-19 13:50:10 +0200

    Complete MVP (without validation)

--- src/components/AddLogEntry.tsx
+++ src/components/AddLogEntry.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { LiftLogEntry } from "../types/LiftTypes";
+import { LiftLogEntry, Rep } from "../types/LiftTypes";
 import "./AddLogEntry.css";
 
 type Props = {
@@ -24,16 +24,33 @@ class AddLogEntry extends React.Component<Props, State> {
       <div className="add-log-entry">
         <div className="row">
           <span className="col">
-            <input type="text" placeholder="Date" />
+            <input
+              type="text"
+              placeholder="Date"
+              onBlur={this.handleDateChanged}
+            />
           </span>
           <span className="col">
-            <input type="text" placeholder="Name" />
+            <input
+              type="text"
+              placeholder="Name"
+              maxLength={50}
+              onBlur={this.handleNameChanged}
+            />
           </span>
           <span className="col">
-            <input type="text" placeholder="Weight" />
+            <input
+              type="text"
+              placeholder="Weight"
+              onBlur={this.handleWeigthLiftedChanged}
+            />
           </span>
           <span className="col">
-            <input type="text" placeholder="Reps" />
+            <input
+              type="text"
+              placeholder="E.g. 3x5 or 5-5-4"
+              onBlur={this.handleRepsChanged}
+            />
           </span>
         </div>
         <button
@@ -46,14 +63,48 @@ class AddLogEntry extends React.Component<Props, State> {
     );
   }
 
-  private addLogEntry() {
-    this.props.onAddEntry({
-      date: new Date(),
-      name: "Test",
-      weightLifted: 1,
-      reps: [{ number: 5 }]
-    });
+  private handleDateChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // validate date
+    this.setState({ date: e.target.value });
+  };
+
+  private handleNameChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ name: e.target.value });
+  };
+
+  private handleWeigthLiftedChanged = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    // validate
+    const weightLifted = Number(e.target.value);
+    this.setState({ weightLifted });
+  };
+
+  private handleRepsChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // validate
+    this.setState({ repsString: e.target.value });
+  };
+
+  private parseReps(repsString: string): Rep[] {
+    if (repsString.indexOf("x") !== -1) {
+      const [sets, reps] = repsString.split("x");
+      return Array(Number(sets)).fill({ number: Number(reps) });
+    }
+
+    const repsArray = repsString.split("-");
+    return repsArray.map(r => ({ number: Number(r) }));
   }
+
+  private addLogEntry = () => {
+    const newEntry = {
+      date: new Date(this.state.date),
+      name: this.state.name,
+      weightLifted: this.state.weightLifted,
+      reps: this.parseReps(this.state.repsString)
+    };
+
+    this.props.onAddEntry(newEntry);
+  };
 }
 
 export default AddLogEntry;

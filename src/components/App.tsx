ce2d5dac23f834ffb426f19018b9c5dc5b0fb98e
Kyrylo Yakymenko <yakimych@gmail.com>
2018-09-30 22:48:55 +0200

    Add error handling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,4 @@
+import { AxiosError } from "axios";
 import * as React from "react";
 import { RouteProps } from "react-router-dom";
 import LiftLogService from "./../services/LiftLogService";
@@ -6,6 +7,10 @@ import "./App.css";
 import LiftLogContainer from "./LiftLogContainer";
 
 type State = {
+  isLoading: boolean;
+  networkErrorOccured: boolean;
+  // TODO: DU?
+  errorMessage: string;
   headerText: string;
   logEntries: LiftLogEntry[];
 };
@@ -18,6 +23,9 @@ class App extends React.Component<RouteProps, State> {
     super(props);
     this.logName = this.getLogNameFromRoute(props);
     this.state = {
+      isLoading: true,
+      networkErrorOccured: false,
+      errorMessage: "",
       headerText: `Loading board ${this.logName}...`,
       logEntries: []
     };
@@ -31,24 +39,50 @@ class App extends React.Component<RouteProps, State> {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">{this.state.headerText}</h1>
+          <h1 className="App-title">
+            {this.state.networkErrorOccured
+              ? this.state.errorMessage
+              : this.state.headerText}
+          </h1>
         </header>
-        <LiftLogContainer
-          entries={this.state.logEntries}
-          onAddEntry={(entry: LiftLogEntry) => this.handleAddEntry(entry)}
-        />
+        {this.state.isLoading || this.state.networkErrorOccured ? null : (
+          <LiftLogContainer
+            entries={this.state.logEntries}
+            onAddEntry={(entry: LiftLogEntry) => this.handleAddEntry(entry)}
+          />
+        )}
       </div>
     );
   }
 
   private reloadLifts() {
-    this.liftLogService.getLiftLog(this.logName).then(liftLog =>
-      this.setState({
-        ...this.state,
-        headerText: liftLog.title,
-        logEntries: liftLog.entries
-      })
-    );
+    this.liftLogService
+      .getLiftLog(this.logName)
+      .then(liftLog =>
+        this.setState({
+          ...this.state,
+          isLoading: false,
+          networkErrorOccured: false,
+          headerText: liftLog.title,
+          logEntries: liftLog.entries
+        })
+      )
+      .catch((error: AxiosError) => {
+        this.setState({
+          ...this.state,
+          isLoading: false,
+          networkErrorOccured: true
+        });
+        if (!!error.response && error.response.status === 404) {
+          this.setState({
+            errorMessage: `Board ${this.logName} does not exist`
+          });
+        } else {
+          this.setState({
+            errorMessage: `An unexpected network error has occured`
+          });
+        }
+      });
   }
 
   private handleAddEntry = (entry: LiftLogEntry) => {

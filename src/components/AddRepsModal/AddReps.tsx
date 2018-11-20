1331b70f5cc81784da03b9fcc420f186d79a3d47
Kyrylo Yakymenko <yakimych@gmail.com>
2018-11-21 00:18:22 +0100

    Test refactoring of a more complex component into an SFC

--- src/components/AddRepsModal/AddReps.tsx
+++ src/components/AddRepsModal/AddReps.tsx
@@ -1,10 +1,5 @@
 import * as React from "react";
-import {
-  InputMode,
-  LiftInfo,
-  LiftLogEntryReps,
-  Set
-} from "../../types/LiftTypes";
+import { InputMode, LiftLogEntryReps, Set } from "../../types/LiftTypes";
 import { formatRepsSets } from "../../utils/LiftUtils";
 import CustomSetsInput from "./CustomSetsInput";
 import InputModeSwitch from "./InputModeSwitch";
@@ -16,78 +11,76 @@ type Props = {
   liftLogReps: LiftLogEntryReps;
 };
 
-class AddReps extends React.Component<Props, {}> {
-  public render() {
-    const {
-      numberOfSets,
-      numberOfReps,
-      customSets,
-      mode
-    } = this.props.liftLogReps;
-    return (
-      <div className="px-1">
-        <div className="d-flex">
-          <InputModeSwitch mode={mode} onChange={this.handleInputModeChange} />
-          <div className="lead ml-4">
-            {formatRepsSets(this.props.liftLogReps)}
-          </div>
-        </div>
-        <div className="my-3">
-          {this.isSetsRepsMode() ? (
-            <SetsRepsInput
-              {...{ numberOfSets, numberOfReps }}
-              onChange={this.handleSetsRepsChange}
-            />
-          ) : (
-            <CustomSetsInput
-              customSets={customSets}
-              onChange={this.handleCustomSetsChange}
-            />
-          )}
-        </div>
-        <LiftInfoContainer
-          onLiftInfoChange={this.onLiftInfoChange}
-          liftInfo={this.props.liftLogReps}
-        />
-      </div>
-    );
+const isSetsRepsMode = (props: Props) =>
+  props.liftLogReps.mode === InputMode.SetsReps;
+
+const getSetsFromNumberSetsReps = (sets: number, reps: number): Set[] =>
+  Array<Set>(sets).fill({ reps, rpe: null });
+
+const handleInputModeChange = (props: Props, mode: InputMode) => {
+  if (mode === InputMode.SetsReps) {
+    props.onLiftLogRepsChange({
+      mode: InputMode.SetsReps
+    } as LiftLogEntryReps);
+  } else {
+    const { numberOfSets, numberOfReps, customSets } = props.liftLogReps;
+    const sets =
+      customSets.length === 0
+        ? getSetsFromNumberSetsReps(numberOfSets, numberOfReps)
+        : customSets;
+
+    props.onLiftLogRepsChange({
+      mode: InputMode.CustomReps,
+      customSets: sets
+    } as LiftLogEntryReps);
   }
-  private onLiftInfoChange = (liftInfo: LiftInfo) =>
-    this.props.onLiftLogRepsChange(liftInfo as LiftLogEntryReps);
+};
 
-  private handleSetsRepsChange = (numberOfSets: number, numberOfReps: number) =>
-    this.props.onLiftLogRepsChange({
-      numberOfSets,
-      numberOfReps
-    });
+const handleSetsRepsChange = (
+  props: Props,
+  numberOfSets: number,
+  numberOfReps: number
+) =>
+  props.onLiftLogRepsChange({
+    numberOfSets,
+    numberOfReps
+  });
 
-  private handleCustomSetsChange = (customSets: Set[]) =>
-    this.props.onLiftLogRepsChange({ customSets } as LiftLogEntryReps);
+const handleCustomSetsChange = (props: Props, customSets: Set[]) =>
+  props.onLiftLogRepsChange({ customSets } as LiftLogEntryReps);
 
-  private handleInputModeChange = (mode: InputMode) => {
-    if (mode === InputMode.SetsReps) {
-      this.props.onLiftLogRepsChange({
-        mode: InputMode.SetsReps
-      } as LiftLogEntryReps);
-    } else {
-      const { numberOfSets, numberOfReps, customSets } = this.props.liftLogReps;
-      const sets =
-        customSets.length === 0
-          ? this.getSetsFromNumberSetsReps(numberOfSets, numberOfReps)
-          : customSets;
+const AddReps: React.SFC<Props> = props => {
+  const { numberOfSets, numberOfReps, customSets, mode } = props.liftLogReps;
 
-      this.props.onLiftLogRepsChange({
-        mode: InputMode.CustomReps,
-        customSets: sets
-      } as LiftLogEntryReps);
-    }
-  };
+  const boundHandleInputModeChange = handleInputModeChange.bind(null, props);
+  const boundHandleSetsRepsChange = handleSetsRepsChange.bind(null, props);
+  const boundHandleCustomSetsChange = handleCustomSetsChange.bind(null, props);
 
-  private isSetsRepsMode = () =>
-    this.props.liftLogReps.mode === InputMode.SetsReps;
-
-  private getSetsFromNumberSetsReps = (sets: number, reps: number): Set[] =>
-    Array<Set>(sets).fill({ reps, rpe: null });
-}
+  return (
+    <div className="px-1">
+      <div className="d-flex">
+        <InputModeSwitch mode={mode} onChange={boundHandleInputModeChange} />
+        <div className="lead ml-4">{formatRepsSets(props.liftLogReps)}</div>
+      </div>
+      <div className="my-3">
+        {isSetsRepsMode(props) ? (
+          <SetsRepsInput
+            {...{ numberOfSets, numberOfReps }}
+            onChange={boundHandleSetsRepsChange}
+          />
+        ) : (
+          <CustomSetsInput
+            customSets={customSets}
+            onChange={boundHandleCustomSetsChange}
+          />
+        )}
+      </div>
+      <LiftInfoContainer
+        onLiftInfoChange={props.onLiftLogRepsChange}
+        liftInfo={props.liftLogReps}
+      />
+    </div>
+  );
+};
 
 export default AddReps;

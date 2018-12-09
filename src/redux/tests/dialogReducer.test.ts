066540e20f63402364bd70c90d1fab11e26f00ed
Kyrylo Yakymenko <yakimych@gmail.com>
2018-12-09 12:30:25 +0100

    Reduce over the dialogReducer in the test

--- src/redux/tests/dialogReducer.test.ts
+++ src/redux/tests/dialogReducer.test.ts
@@ -2,25 +2,30 @@ import { AnyAction } from "redux";
 import { InputMode } from "../../types/LiftTypes";
 import { actions } from "../dialogActions";
 import { dialogReducer } from "../dialogReducer";
+import { DialogState } from "../storeState";
+
+// Let it default to the reducer's initialState
+const emptyInitialState = (undefined as unknown) as DialogState;
+const emptyInitialAction = {} as AnyAction;
 
 it("should be closed from the start", () => {
-  const dialogState = dialogReducer(undefined, {} as AnyAction);
+  const dialogState = dialogReducer(emptyInitialState, emptyInitialAction);
   expect(dialogState.isOpen).toEqual(false);
 });
 
 it("should be open after passing 'open' action", () => {
-  const dialogState = dialogReducer(undefined, actions.open());
+  const dialogState = dialogReducer(emptyInitialState, actions.open());
   expect(dialogState.isOpen).toEqual(true);
 });
 
 it("should be closed after passing 'close' action", () => {
-  const openDialogState = dialogReducer(undefined, actions.open());
+  const openDialogState = dialogReducer(emptyInitialState, actions.open());
   const dialogState = dialogReducer(openDialogState, actions.close());
   expect(dialogState.isOpen).toEqual(false);
 });
 
 it("numberOfSets should be set to correct numeric value", () => {
-  const openDialogState = dialogReducer(undefined, actions.open());
+  const openDialogState = dialogReducer(emptyInitialState, actions.open());
   const dialogStateWith10Sets = dialogReducer(
     openDialogState,
     actions.setNumberOfSets("10")
@@ -30,7 +35,7 @@ it("numberOfSets should be set to correct numeric value", () => {
 });
 
 it("numberOfReps should be set to correct numeric value", () => {
-  const openDialogState = dialogReducer(undefined, actions.open());
+  const openDialogState = dialogReducer(emptyInitialState, actions.open());
   const dialogStateWith11Reps = dialogReducer(
     openDialogState,
     actions.setNumberOfReps("11")
@@ -40,51 +45,42 @@ it("numberOfReps should be set to correct numeric value", () => {
 });
 
 it("customsets should be derived from SetsReps after toggling InputMode to Custom", () => {
-  const openDialogState = dialogReducer(undefined, actions.open());
-  const dialogStateWith2Sets = dialogReducer(
-    openDialogState,
-    actions.setNumberOfSets("2")
-  );
-  const dialogStateWith8Reps = dialogReducer(
-    dialogStateWith2Sets,
-    actions.setNumberOfReps("8")
-  );
-  const dialogState = dialogReducer(
-    dialogStateWith8Reps,
+  const allActions = [
+    actions.open(),
+    actions.setNumberOfSets("2"),
+    actions.setNumberOfReps("8"),
     actions.setInputMode(InputMode.CustomReps)
+  ];
+
+  const finalState = allActions.reduce<DialogState>(
+    dialogReducer,
+    emptyInitialState
   );
-  expect(dialogState.customSetsStrings).toEqual(["8", "8"]);
-  expect(dialogState.customSets).toEqual([
+
+  expect(finalState.customSetsStrings).toEqual(["8", "8"]);
+  expect(finalState.customSets).toEqual([
     { reps: 8, rpe: null },
     { reps: 8, rpe: null }
   ]);
 });
 
-// TODO: Is there a way to chain reducer calls?
 it("customsets should be not be derived from SetsReps if set previously", () => {
-  const openDialogState = dialogReducer(undefined, actions.open());
-  const dialogStateWithCustomInputMode = dialogReducer(
-    openDialogState,
+  const allActions = [
+    actions.open(),
+    actions.setInputMode(InputMode.CustomReps),
+    actions.changeCustomSet({ index: 0, value: "8@9.5" }),
+    actions.addCustomSet(),
+    actions.setInputMode(InputMode.SetsReps),
     actions.setInputMode(InputMode.CustomReps)
+  ];
+
+  const finalState = allActions.reduce<DialogState>(
+    dialogReducer,
+    emptyInitialState
   );
-  const tempState1 = dialogReducer(
-    dialogStateWithCustomInputMode,
-    actions.changeCustomSet({ index: 0, value: "8@9.5" })
-  );
-  const dialogStateWithCustomSetsReps = dialogReducer(
-    tempState1,
-    actions.addCustomSet()
-  );
-  const tempState2 = dialogReducer(
-    dialogStateWithCustomSetsReps,
-    actions.setInputMode(InputMode.SetsReps)
-  );
-  const dialogState = dialogReducer(
-    tempState2,
-    actions.setInputMode(InputMode.CustomReps)
-  );
-  expect(dialogState.customSetsStrings).toEqual(["8@9.5", "5", "5", "5"]);
-  expect(dialogState.customSets).toEqual([
+
+  expect(finalState.customSetsStrings).toEqual(["8@9.5", "5", "5", "5"]);
+  expect(finalState.customSets).toEqual([
     { reps: 8, rpe: 9.5 },
     { reps: 5, rpe: null },
     { reps: 5, rpe: null },

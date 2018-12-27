54e0ef02434d2f8ad9492fe84a720d29af3c5c06
Kyrylo Yakymenko <yakimych@gmail.com>
2018-12-27 13:54:13 +0100

    Make CustomSetsInput into a functional component

--- src/components/AddRepsModal/CustomSetsInput.tsx
+++ src/components/AddRepsModal/CustomSetsInput.tsx
@@ -15,77 +15,49 @@ type Props = {
   customSets: ReadonlyArray<Set>;
 };
 
-// TODO: Remove
-type State = {
-  customSets: string[];
-};
+// TODO: Move this check to the reducer?
+const isAddSetDisabled = (numberOfCustomSets: number) =>
+  numberOfCustomSets >= MAX_REP_SET_VALUE;
 
-class CustomSetsInput extends React.Component<Props, State> {
-  public state: Readonly<State> = {
-    customSets: this.props.customSets.map(formatSet)
-  };
-
-  public render() {
-    return (
-      <>
-        <div className="d-flex flex-wrap">
-          {this.state.customSets.map((formattedSet, index) => (
-            <div key={index} className="custom-sets-input-group mr-1 mb-1">
-              <InputGroup>
-                <Input
-                  className="set-rep-input"
-                  bsSize="sm"
-                  value={formattedSet}
-                  onChange={e => this.handleRepValueChanged(e, index)}
-                  // TODO: Do we need this?
-                  // onBlur={e => this.handleRepValueChangeCompleted(e, index)}
-                />
-                {index !== 0 && (
-                  <InputGroupAddon addonType="append">
-                    <div
-                      className="input-group-text remove-icon-wrapper p-0"
-                      onClick={() => this.handleRemoveSetClick(index)}
-                    >
-                      <Octicon icon={getIconByName("x")} />
-                    </div>
-                  </InputGroupAddon>
-                )}
-              </InputGroup>
-            </div>
-          ))}
+const CustomSetsInput: React.SFC<Props> = props => (
+  <>
+    <div className="d-flex flex-wrap">
+      {props.customSets.map(formatSet).map((formattedSet, index) => (
+        <div key={index} className="custom-sets-input-group mr-1 mb-1">
+          <InputGroup>
+            <Input
+              className="set-rep-input"
+              bsSize="sm"
+              value={formattedSet}
+              onChange={e => props.onChange(index, e.target.value)}
+              // TODO: Do we need this?
+              // onBlur={e => this.handleRepValueChangeCompleted(e, index)}
+            />
+            {index !== 0 && (
+              <InputGroupAddon addonType="append">
+                <div
+                  className="input-group-text remove-icon-wrapper p-0"
+                  onClick={() => props.onRemove(index)}
+                >
+                  <Octicon icon={getIconByName("x")} />
+                </div>
+              </InputGroupAddon>
+            )}
+          </InputGroup>
         </div>
+      ))}
+    </div>
 
-        <Button
-          color="success"
-          size="sm"
-          className="mt-2"
-          onClick={this.handleAddSetClick}
-          disabled={this.isAddSetDisabled()}
-        >
-          <Octicon icon={getIconByName("plus")} />
-        </Button>
-      </>
-    );
-  }
-
-  private isAddSetDisabled = () =>
-    this.props.customSets.length >= MAX_REP_SET_VALUE;
-
-  private handleAddSetClick = () => {
-    this.props.onAdd();
-  };
-
-  private handleRepValueChanged = (
-    e: React.ChangeEvent<HTMLInputElement>,
-    index: number
-  ) => {
-    const newSetStringValue = e.target.value;
-    this.props.onChange(index, newSetStringValue);
-  };
-
-  private handleRemoveSetClick = (index: number) => {
-    this.props.onRemove(index);
-  };
-}
+    <Button
+      color="success"
+      size="sm"
+      className="mt-2"
+      onClick={props.onAdd}
+      disabled={isAddSetDisabled(props.customSets.length)}
+    >
+      <Octicon icon={getIconByName("plus")} />
+    </Button>
+  </>
+);
 
 export default CustomSetsInput;
